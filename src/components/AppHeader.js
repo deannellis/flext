f177e126591a666cb40e8fe8d4c318d6de985d4f
Nellis,Dean <dean.nellis@lexmark.com>
2020-02-01 15:36:57 -0500

    Added svg icons

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { HamburgerIcon } from '../utils/icons';
 
 const AppHeader = ({ toggleMenu }) => {
     return (
         <div className="app-header">
             <button className="app-header__hamburger-button" onClick={() => {toggleMenu()}} >
-                <i className="fas fa-bars"></i>
+                {/* <i className="fas fa-bars"></i> */}
+                <HamburgerIcon size={24} />
             </button>
             <h1>flext</h1>
         </div>
