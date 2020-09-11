ed5f6b906dd45d3710530e2ceec161f365613af9
Nellis,Dean <dean.nellis@lexmark.com>
2020-09-11 16:58:59 -0400

    Adding new files that didn't get picked up in last commit

---
+++ src/pages/LoadingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+	return (
+		<div className="loading-page">
+			<img alt="loading spinner" src="./images/Spinner-1s-200px.gif" />
+		</div>
+	);
+};
+
+export default LoadingPage;
